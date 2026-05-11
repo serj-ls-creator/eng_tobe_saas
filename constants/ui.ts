e591b0ed7a33e3400c76c5cf1123d2e9adf157d7
Serhii <serj127@gmail.com>
2026-05-11 19:53:41 +0300

    при регмисир не пишу чтобы проверяли почту

--- constants/ui.ts
+++ constants/ui.ts
@@ -25,7 +25,7 @@ export const UI_TEXT = {
   fullNameLabel: "Full name",
   authLoginButton: "Sign in",
   authSignupButton: "Sign up",
-  authSignupSuccess: "Email sent! Please confirm it in your inbox",
+  authSignupSuccess: "Sign up successful.",
   authSwitchToSignup: "Create account",
   authSwitchToLogin: "Back to login",
   lockedLabel: "Premium",
